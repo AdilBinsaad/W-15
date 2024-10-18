914089f53e376a858f852a00c692416b6b07fe65
AdilBinsaad <adilbinsaad@gmail.com>
2024-10-18 15:53:56 +0700

    Add Lab15

---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using CalculatorLibrary;
+
+namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        int c = CalculatorLibrary.CalculatorLibrary.Add(a,b);
+        Console.WriteLine($"Result from Add({a},{b}) is {c}");
+    }
+}
